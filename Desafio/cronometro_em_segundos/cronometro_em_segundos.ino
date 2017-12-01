a6b4e5b3f38596a1cd0c504630b24ee75ed74aad
Waldo Costa <waldooo@users.noreply.github.com>
2017-12-01 16:56:15 -0200

    Add files via upload

---
+++ Desafio/cronometro_em_segundos/cronometro_em_segundos.ino
@@ -0,0 +1,36 @@
+int pinbutton1 = 2;
+int pinbutton2 = 3;
+
+float inicio = 0;
+float fim = 0;
+float total = 0;
+
+void setup() {
+pinMode(pinbutton1, INPUT_PULLUP);
+pinMode(pinbutton2, INPUT_PULLUP);
+
+Serial.begin(9600);
+}
+
+void loop() {
+
+if (digitalRead(pinbutton1)==0)
+{
+  inicio = millis();
+}
+if (digitalRead(pinbutton2)==0)
+{
+  fim = millis();
+}
+
+if (inicio > 0 && fim > 0 )
+{
+  total = (fim - inicio)/1000;
+  Serial.println(total);
+
+  inicio = 0;
+  fim = 0 ;
+  total = 0;
+}
+
+}
